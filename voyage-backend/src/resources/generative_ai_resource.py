4fdd7ffdefb8432092523d9ff6509d665a6094e2
netaw <netaw@qwilt.com>
2024-04-18 09:33:54 +0300

    -

--- voyage-backend/src/resources/generative_ai_resource.py
+++ voyage-backend/src/resources/generative_ai_resource.py
@@ -1,5 +1,11 @@
 import google.generativeai as genai
-genai.configure(api_key="AIzaSyAQP4QC0fJJB6tOmjZ-FV4YPSTR11rbJdQ")
+import os
+from dotenv import load_dotenv
+
+# define google model properties: API key through the environment variable and the required model
+load_dotenv()
+google_api_key = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=google_api_key)
 model = genai.GenerativeModel('gemini-pro')
 
 
