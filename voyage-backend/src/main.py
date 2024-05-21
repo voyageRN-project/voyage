0d628249bf702ebb5c9a84f2c2af1bc2f051e405
netaw <netaw@qwilt.com>
2024-05-21 14:27:58 +0300

    logo size update

--- voyage-backend/src/main.py
+++ voyage-backend/src/main.py
@@ -1,12 +1,14 @@
 from services.business_clients_service import BusinessService
 import traceback
 from flask import Flask, request, Response, jsonify
+from flask_cors import CORS
 from services.user_service import UserService
 from helpers.error_handling import MissingExpectedKeyInRequestBodyError, CouldNotGetValidResponseFromThirdParty, ConvertAIResponseToJsonError
 import logging as logger
 logger.basicConfig(level=logger.INFO)
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/api/v1/business_app/add_business", methods=['POST'])
