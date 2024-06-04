5db91ff5406e26e6292f495a95d047784925ba25
netaw <netaw@qwilt.com>
2024-06-04 17:11:13 +0300

    adding readme file

--- voyage-backend/src/processors/data_validator.py
+++ voyage-backend/src/processors/data_validator.py
@@ -3,6 +3,7 @@ from typing import Union, Any
 from ..helpers.error_handling import ThirdPartyDataValidatorError
 from ..helpers.error_handling import CountryNameError
 from geopy.geocoders import Nominatim
+import reverse_geocoder as rg
 import requests
 import json
 import geopy
@@ -47,7 +48,10 @@ class DataValidator:
         except Exception as e:
             logger.error(f"Could not get location from the third party location validator: {str(e)}, returning true "
                          f"to proceed")
-            return True
+            coordinates = (float(latitude), float(longitude))
+            results = rg.search(coordinates)
+            if c_code.lower() not in results[0]['cc'].lower():
+                return False
         if c_code.lower() not in res.raw['address']['country_code'].lower():
             return False
         return True
